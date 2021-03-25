514b400fe6d02ce8fcd718a46a3035e2223e1e43
AleksanderTimonov <78326181+AleksanderTimonov@users.noreply.github.com>
2021-03-25 12:00:29 -0700

    Update playerControl.cs
    Added movement on all axes

--- playerControl.cs
+++ playerControl.cs
@@ -17,12 +17,32 @@ public class playerControl : MonoBehaviour
     [SerializeField]
     KeyCode rightArrow;
 
+    private void Start()
+    {
+        verticalMovement.y = 0.1f;
+        horizontalMovement.x = 0.1f;
+        upArrow = KeyCode.UpArrow;
+        downArrow = KeyCode.DownArrow;
+        leftArrow = KeyCode.LeftArrow;
+        rightArrow = KeyCode.RightArrow;
+    }
+
     void FixedUpdate()
     {
+
         if (Input.GetKey(upArrow))
             GetComponent<Transform>().position += new Vector3(0, verticalMovement.y, 0);
 
-        
+        if (Input.GetKey(downArrow))
+            GetComponent<Transform>().position -= new Vector3(0, verticalMovement.y, 0);
+
+        if (Input.GetKey(leftArrow))
+            GetComponent<Transform>().position -= new Vector3(horizontalMovement.x, 0, 0);
+
+        if (Input.GetKey(rightArrow))
+            GetComponent<Transform>().position += new Vector3(horizontalMovement.x, 0, 0);
+
+
 
 
     }
