c175a950e3bd3a77ba8d98af8f756eb34b3efebc
AleksanderTimonov <78326181+AleksanderTimonov@users.noreply.github.com>
2021-03-25 15:14:37 -0700

    Update playerControl.cs
    Changed some labels and stuff.

--- playerControl.cs
+++ playerControl.cs
@@ -17,7 +17,7 @@ public class playerControl : MonoBehaviour
     [SerializeField]
     KeyCode rightArrow;
 
-    private void Start()
+    public void Start()
     {
         verticalMovement.y = 0.1f;
         horizontalMovement.x = 0.1f;
@@ -30,17 +30,30 @@ public class playerControl : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (Input.GetKey(upArrow))
+        
+        if (Input.GetKey(upArrow)) // NORTH
+        {
             GetComponent<Transform>().position += new Vector3(0, verticalMovement.y, 0);
 
-        if (Input.GetKey(downArrow))
+
+        }
+        if (Input.GetKey(downArrow)) //SOUTH
+        {
             GetComponent<Transform>().position -= new Vector3(0, verticalMovement.y, 0);
 
-        if (Input.GetKey(leftArrow))
+        }
+
+        if (Input.GetKey(leftArrow)) // WEST
+        {
             GetComponent<Transform>().position -= new Vector3(horizontalMovement.x, 0, 0);
 
-        if (Input.GetKey(rightArrow))
+        }
+
+        if (Input.GetKey(rightArrow)) //EAST
+        {
             GetComponent<Transform>().position += new Vector3(horizontalMovement.x, 0, 0);
+
+        }
 
 
 
